7493846292c1cdafc991506724b13931ac3950b0
Yuxuan Li <54911077+victoriali8023@users.noreply.github.com>
2021-03-28 16:34:40 -0400

    update

--- dummyData.js
+++ dummyData.js
@@ -14,6 +14,19 @@ const dummyCustomers = [
 		city: "Ann Arbor",
 		state: "MI",
 		zipcode: 48105,
+		vehicles: {
+			create: [
+				{
+					vin: "1234567890",
+					vehicleType: "Sedan",
+					year: 2000,
+					make: "Ford",
+					model: "Focus SE",
+					imgUrl:
+						"https://file.kelleybluebookimages.com/kbb/base/evox/CP/10858/2017-Ford-Focus-front_10858_032_2400x1800_YZ.png",
+				},
+			],
+		},
 	},
 	{
 		firstName: "Jane",
@@ -26,6 +39,19 @@ const dummyCustomers = [
 		city: "Ypsilanti",
 		state: "MI",
 		zipcode: 48197,
+		vehicles: {
+			create: [
+				{
+					vin: "0987654321",
+					vehicleType: "SUV",
+					year: 2010,
+					make: "Chevrolet",
+					model: "Trailblazer",
+					imgUrl:
+						"https://www.gannett-cdn.com/presto/2020/07/10/PDTF/76f14475-53f5-4abe-ae0f-a4f4911c8be3-IMG_2481.JPG",
+				},
+			],
+		},
 	},
 	{
 		firstName: "Ben",
@@ -38,67 +64,29 @@ const dummyCustomers = [
 		city: "Detroit",
 		state: "MI",
 		zipcode: 48201,
-	},
-];
-
-const dummyVehicles = [
-	{
-		vin: "1234567890",
-		type: "Sedan",
-		year: 2000,
-		make: "Ford",
-		model: "Focus SE",
-		imgUrl:
-			"https://file.kelleybluebookimages.com/kbb/base/evox/CP/10858/2017-Ford-Focus-front_10858_032_2400x1800_YZ.png",
-	},
-	{
-		vin: "0987654321",
-		type: "SUV",
-		year: 2010,
-		make: "Chevrolet",
-		model: "Trailblazer",
-		imgUrl:
-			"https://www.gannett-cdn.com/presto/2020/07/10/PDTF/76f14475-53f5-4abe-ae0f-a4f4911c8be3-IMG_2481.JPG",
-	},
-	{
-		vin: "1122334455",
-		type: "Truck",
-		year: 2005,
-		make: "Toyota",
-		model: "Tundra",
-		imgUrl:
-			"https://www.toyota.com/imgix/responsive/images/mlp/colorizer/2021/tundra/8W2/1.png",
+		vehicles: {
+			create: [
+				{
+					vin: "1122334455",
+					vehicleType: "Truck",
+					year: 2005,
+					make: "Toyota",
+					model: "Tundra",
+					imgUrl:
+						"https://www.toyota.com/imgix/responsive/images/mlp/colorizer/2021/tundra/8W2/1.png",
+				},
+			],
+		},
 	},
 ];
 
 async function main() {
-	let customerVehicles = [];
-
-	// Create a new character
+	// Create a new customer
 	for (let customer of dummyCustomers) {
 		const newCustomer = await prisma.customer.create({ data: customer });
 		console.log(
-			`Created new customer: ${newCustomer.name} (ID: ${
-				newCustomer.customerID
-			})`
+			`Created new customer: ${newCustomer.firstName} (ID: ${newCustomer.id})`
 		);
-		customerVehicles.push({ customerID: newCustomer.customerID });
-	}
-
-	for (let i = 0; i < dummyVehicles.length; i++) {
-		const newVehicle = await prisma.vehicle.create({ data: dummyVehicles[i] });
-		console.log(
-			`Created new vehicle: ${newVehicle.make + newVehicle.model} (ID: ${
-				newVehicle.vehicleID
-			})`
-		);
-		customerVehicles[i].vehicleID = newVehicle.vehicleID;
-	}
-
-	for (let dummyCustomerVehicle of customerVehicles) {
-		const newCustomer = await prisma.customerVehicle.create({
-			data: dummyCustomerVehicle,
-		});
 	}
 }
 
